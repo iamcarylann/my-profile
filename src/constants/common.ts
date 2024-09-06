fdaba4ef211aa27927af43fb6736930ebd2f84f6
iamcarylann <iamcarylann@gmail.com~>
2024-09-06 08:28:32 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Caryl Ann Apepe';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'IT student intern with hands-on experience in software development, including writing, testing, debugging, and maintaining code to create functional applications and systems. Knowledgeable in programming languages, algorithms, data structures, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Focused on developing and maintaining web applications, working closely with cross-functional teams to design, implement, and deploy scalable solutions that align with project and business goals.';
+export const ABOUT_TEXT_3 =
+  'Versatile IT intern with proficiency in both frontend and backend development, capable of contributing to the building of complete web applications. Strong foundation in various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'IT student intern with experience in building websites and web applications using modern JavaScript tools/frameworks. Solid understanding of front-end and back-end technologies, along with database management.';
+export const RESUME_TEXT_2 =
+  'IT student intern with experience in building websites and web applications using modern JavaScript tools/frameworks. Contributed to projects involving both frontend and backend development.';
